32979a61320beca3615b04f2314d8c1302a5bbb9
bingoyu <yaojunchina@qq.com>
2018-08-23 21:41:05 +0800

    update calico tpl

--- scripts/kubeadm-ha.sh
+++ scripts/kubeadm-ha.sh
@@ -3,8 +3,7 @@
 
 set -eu
 
-: ${K8S_M2:="k8s-m2"}
-: ${K8S_M3:="k8s-m3"}
-
-ssh ${K8S_M2} < kubeadm-m2.sh
-ssh ${K8S_M3} < kubeadm-m3.sh
+echo "===install the sencond master==="
+ssh k8s-m2 < kubeadm-m2.sh
+echo "===install the third master==="
+ssh k8s-m3 < kubeadm-m3.sh
