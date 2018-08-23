f314afe22cff8e9535f8ee5c12df7c69840f0ae3
bingoyu <yaojunchina@qq.com>
2018-08-23 15:03:33 +0800

    set path

--- scripts/firewall.sh
+++ scripts/firewall.sh
@@ -7,10 +7,10 @@ echo ""
 echo "===set up masters' firewall==="
 export NODES="k8s-m1 k8s-m2 k8s-m3"
 export PORTS="16443 6443 4001 2379-2380 10250 10251 10252 10255 30000-32767"
-./firewall-setup.sh
+sh firewall-setup.sh
 
 echo ""
 echo "===set up nodes' firewall==="
 export NODES="k8s-n1"
 export PORTS="10250 30000-32767"
-./firewall-setup.sh
+sh firewall-setup.sh
