5ad24d614ba63cce60656bc16050ce1843cd9295
bingoyu <yaojunchina@qq.com>
2018-09-04 19:29:19 +0800

    add export NODES

--- scripts/kubeadm-up.sh
+++ scripts/kubeadm-up.sh
@@ -4,12 +4,14 @@
 set -eu
 
 echo "===init first master==="
+export NODES="k8s-m2 k8s-m3"
 sh kubeadm-init.sh
 
 echo "===sleep 120s wait pod etc-k8s-m1 up==="
 sleep 120
 
 echo "===install other masters==="
+export NODES="k8s-m2 k8s-m3"
 sh kubeadm-ha.sh
 
 echo "===install addons on masters==="
